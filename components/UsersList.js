d519a356d679aa237d487f4fa4d10e3f3b788e67
yogesh109202 <128414694+yogesh109202@users.noreply.github.com>
2024-01-30 22:17:34 +0530

    Update UsersList.js

--- components/UsersList.js
+++ components/UsersList.js
@@ -1,13 +1,19 @@
-import { useMemo } from 'react';
+import React, { useMemo } from "react";
 
 function UsersList({ users, startingLetter }) {
-  
+  const filteredUsers = useMemo(() => {
+    return users.filter((user) => {
+      return user.name[0].toLowerCase() === startingLetter.toLowerCase();
+    });
+  }, [users, startingLetter]);
 
   return (
     <ul>
-      <li key={}>{}</li>
+      {filteredUsers.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
     </ul>
   );
 }
 
-export default UsersList;
+export default React.memo(UsersList);
