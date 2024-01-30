bcdd44b9c50e9475ccdd05969c7eb8251b3aa146
yogesh109202 <128414694+yogesh109202@users.noreply.github.com>
2024-01-30 22:16:48 +0530

    Initial commit

---
+++ components/UsersList.js
@@ -0,0 +1,13 @@
+import { useMemo } from 'react';
+
+function UsersList({ users, startingLetter }) {
+  
+
+  return (
+    <ul>
+      <li key={}>{}</li>
+    </ul>
+  );
+}
+
+export default UsersList;
